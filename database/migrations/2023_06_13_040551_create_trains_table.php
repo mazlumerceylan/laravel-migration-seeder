bf86c871341dc6c9578689b1dbb5ddd8f18cb872
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-06-13 06:08:19 +0200

    add column in up

--- database/migrations/2023_06_13_040551_create_trains_table.php
+++ database/migrations/2023_06_13_040551_create_trains_table.php
@@ -12,21 +12,21 @@ return new class extends Migration
      * @return void
      */
     public function up()
-{
-    Schema::create('trains', function (Blueprint $table) {
-        $table->id();
-        $table->string('company');
-        $table->string('departure_station');
-        $table->string('arrival_station');
-        $table->time('departure_time');
-        $table->time('arrival_time');
-        $table->string('train_code');
-        $table->integer('num_carriages');
-        $table->boolean('on_time')->default(true);
-        $table->boolean('cancelled')->default(false);
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code');
+            $table->integer('num_carriages');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
 
 
     /**
