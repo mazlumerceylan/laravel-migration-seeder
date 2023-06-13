dbd7998c52aef376425e5ddf5cf2e4c96d0b449c
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-06-13 06:07:37 +0200

    first commit

---
+++ database/migrations/2023_06_13_040551_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+{
+    Schema::create('trains', function (Blueprint $table) {
+        $table->id();
+        $table->string('company');
+        $table->string('departure_station');
+        $table->string('arrival_station');
+        $table->time('departure_time');
+        $table->time('arrival_time');
+        $table->string('train_code');
+        $table->integer('num_carriages');
+        $table->boolean('on_time')->default(true);
+        $table->boolean('cancelled')->default(false);
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
